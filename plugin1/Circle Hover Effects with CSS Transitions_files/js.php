6484284ea58f0fc0c8dcb6060d25cc077cfe2e99
Furqan <Furqan@Furqans-MacBook-Pro-2.local>
2013-11-15 22:40:35 +0500

    ádding plugins

---
+++ plugin1/Circle Hover Effects with CSS Transitions_files/js.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+	<!--[if lt IE 7 ]><html dir="ltr" lang="en-US" class="no-js ie ie6 lte7 lte8 lte9"><![endif]-->
+	<!--[if IE 7 ]><html dir="ltr" lang="en-US" class="no-js ie ie7 lte7 lte8 lte9"><![endif]-->
+	<!--[if IE 8 ]><html dir="ltr" lang="en-US" class="no-js ie ie8 lte8 lte9"><![endif]-->
+	<!--[if IE 9 ]><html dir="ltr" lang="en-US" class="no-js ie ie9 lte9"><![endif]-->
+	<!--[if (gt IE 9)|!(IE)]><!--><html dir="ltr" lang="en-US"> <!--<![endif]-->
+	<head><script type="text/javascript">var NREUMQ=NREUMQ||[];NREUMQ.push(["mark","firstbyte",new Date().getTime()]);</script><link rel="stylesheet" type="text/css" href="http://codropspz.tympanus.netdna-cdn.com/codrops/wp-content/cache/minify/000000/HclbCoAwDATAC1lSvVFJ4wNsE3YD4u0F53dWeRw9YGTZHaOMNtthECWF-d62VFHv8GCeNqxuf02n4or8AA.css" media="all" />
+
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Nothing found for  Codrops Wp-Content Plugins Oiopub-Direct Js Php?type=Banner&amp;align=Center&amp;zone=1</title>
+				<link rel="stylesheet" type="text/css" href="http://codropspz.tympanus.netdna-cdn.com/codrops/wp-content/themes/codropstheme02/css/codrops.css?v2" />
+		<link rel="stylesheet" type="text/css" media="print" href="http://codropspz.tympanus.netdna-cdn.com/codrops/wp-content/themes/codropstheme02/css/codrops_print.css" />
+		<link rel="profile" href="http://gmpg.org/xfn/11" />
+		<link rel="shortcut icon" href="http://codropspz.tympanus.netdna-cdn.com/codrops/wp-content/themes/codropstheme02/favicon.ico">
+
+		 
+		
+			   
+		
+<script type='text/javascript'>
+/* <![CDATA[ */
+var fm_user_I18n = {"ajaxurl":"http:\/\/tympanus.net\/codrops\/wp-admin\/admin-ajax.php"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://codropspz.tympanus.netdna-cdn.com/codrops/wp-content/plugins/wordpress-form-manager/js/userscripts.js?ver=3.6.1'></script>
+<script type='text/javascript' src='http://tympanus.net/codrops/wp-includes/js/jquery/jquery.js?ver=1.10.2'></script>
+<script type='text/javascript' src='http://tympanus.net/codrops/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://tympanus.net/codrops/xmlrpc.php?rsd" />
+
+<!-- All in One SEO Pack 2.0.4 by Michael Torbert of Semper Fi Web Design[637,651] -->
+<!-- /all in one seo pack -->
+		<script src="http://codropspz.tympanus.netdna-cdn.com/codrops/wp-content/themes/codropstheme02/js/search.js"></script>
+
+		<script type="text/javascript">
+			  var _gaq = _gaq || [];
+			  _gaq.push(['_setAccount', 'UA-7243260-2']);
+			  _gaq.push(['_trackPageview']);
+
+			  (function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			  })();
+		</script>
+
+				<script type="text/javascript" src="http://codropspz.tympanus.netdna-cdn.com/codrops/wp-content/themes/codropstheme02/js/modernizr.custom.70736.js"></script>
+		<!--[if lt IE 9 ]>
+			<script type="text/javascript" src="http://codropspz.tympanus.netdna-cdn.com/codrops/wp-content/themes/codropstheme02/js/css3-mediaqueries.js"></script>
+			<link rel="stylesheet" href="http://codropspz.tympanus.netdna-cdn.com/codrops/wp-content/themes/codropstheme02/css/ie.css"/>
+		<![endif]-->
+		<!--[if lt IE 8 ]>
+			<script type="text/javascript" src="http://codropspz.tympanus.netdna-cdn.com/codrops/wp-content/themes/codropstheme02/js/iconfonts.js"></script>
+		<![endif]-->
+		<noscript>
+			
+		</noscript>
+	</head>
+	<body>
+		<!-- BuySellAds.com Ad Code -->
+		<script type="text/javascript">
+			(function(){
+				var bsa = document.createElement('script');
+				bsa.type = 'text/javascript';
+				bsa.async = true;
+				bsa.src = '//s3.buysellads.com/ac/bsa.js';
+				(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);
+			})();
+		</script>
+		<!-- END BuySellAds.com Ad Code -->	
+		<div class="ct-container">
+			<header class="ct-header">
+				<div class="ct-inner ct-cf">
+
+											<div class="ct-branding">
+							<a class="ct-logo" href="/codrops" rel="home">Codrops</a>
+						</div>
+					
+					<div class="ct-nav-wrapper">
+						<nav class="ct-nav-main">
+						   <ul>
+								<li><a href="/codrops/category/tutorials/">Tutorials</a></li>
+								<li><a href="/codrops/category/articles/">Articles</a></li>
+								<li><a href="/codrops/category/playground/">Playground</a></li>
+								<li><a href="/codrops/category/blueprints/">Blueprints</a></li>
+								<li><a href="/codrops/collective/">Collective</a></li>
+							</ul>
+						</nav>
+					</div>
+
+					<div class="ct-header-items-right ct-cf">
+						<div class="ct-search" id="ct-search">
+							<form method="get" id="searchform" action="http://tympanus.net/codrops/">
+								<div class="ct-search-input-wrap"><input class="ct-search-input" placeholder="Search on Codrops..." type="text" value="" name="s" id="s"/></div>
+								<input class="ct-search-submit" type="submit" id="go" value=""><span class="ct-icon-search"></span>
+							</form>
+						</div>
+						<ul class="ct-connect">
+							<li><a class="ct-icon-feed" href="http://feeds2.feedburner.com/tympanus"><span>Rss Feed</span></a></li>
+							<li><a class="ct-icon-mail" href="http://feedburner.google.com/fb/a/mailverify?uri=tympanus&loc=en_US"><span>Email Updates</span></a></li>
+							<li><a class="ct-icon-twitter" href="http://www.twitter.com/codrops"><span>Codrops on Twitter</span></a></li>
+							<li><a class="ct-icon-facebook" href="http://www.facebook.com/pages/Codrops/159107397912"><span>Codrops on Facebook</span></a></li>
+							<li><a class="ct-icon-google-plus" href="https://plus.google.com/101095823814290637419" rel="publisher"><span>Codrops on Google+</span></a></li>
+						    <li><a class="ct-icon-github" href="https://github.com/codrops"><span>Codrops on Github</span></a></li>
+						</ul>
+					</div><!--/ct-header-items-right-->
+
+				</div><!-- ct-inner -->
+			</header><div class="ct-content">
+    <div class="ct-inner">
+        <div class="ct-main" id="ct-main" role="main">
+			<article class="ct-notfound">
+				<h1>Tony, what is this 404 thing?</h1>
+				<iframe width="820" height="447" src="http://www.youtube.com/embed/yHzh0PvMWTI?rel=0" frameborder="0" allowfullscreen></iframe>
+				<h2>More sweet cannoli here:</h2>
+				<ul class="related_post">
+					<li><a href='http://tympanus.net/codrops/2013/10/30/medium-style-page-transition/' title='Medium-Style Page Transition'>Medium-Style Page Transition</a></li>
+	<li><a href='http://tympanus.net/codrops/2013/10/29/multi-item-selection/' title='Multi-Item Selection'>Multi-Item Selection</a></li>
+	<li><a href='http://tympanus.net/codrops/2013/10/25/split-layout/' title='Split Layout'>Split Layout</a></li>
+	<li><a href='http://tympanus.net/codrops/2013/10/23/animations-for-thumbnail-grids/' title='Animations for Thumbnail Grids'>Animations for Thumbnail Grids</a></li>
+	<li><a href='http://tympanus.net/codrops/2013/10/15/animated-checkboxes-and-radio-buttons-with-svg/' title='Animated Checkboxes and Radio Buttons with SVG'>Animated Checkboxes and Radio Buttons with SVG</a></li>
+	<li><a href='http://tympanus.net/codrops/2013/10/03/a-collection-of-separator-styles/' title='A Collection of Separator Styles'>A Collection of Separator Styles</a></li>
+	<li><a href='http://tympanus.net/codrops/2013/09/30/animated-border-menus/' title='Animated Border Menus'>Animated Border Menus</a></li>
+	<li><a href='http://tympanus.net/codrops/2013/09/23/so-youre-a-web-designer-right/' title='So, You&#8217;re a Web Designer, Right?'>So, You&#8217;re a Web Designer, Right?</a></li>
+	<li><a href='http://tympanus.net/codrops/2013/09/18/creative-loading-effects/' title='Creative Loading Effects'>Creative Loading Effects</a></li>
+	<li><a href='http://tympanus.net/codrops/2013/09/05/sliding-horizontal-layout/' title='Sliding Horizontal Layout'>Sliding Horizontal Layout</a></li>
+	<li><a href='http://tympanus.net/codrops/2013/08/28/transitions-for-off-canvas-navigations/' title='Transitions for Off-Canvas Navigations'>Transitions for Off-Canvas Navigations</a></li>
+	<li><a href='http://tympanus.net/codrops/2013/08/27/3d-shading-with-box-shadows/' title='3D Shading with Box-Shadows'>3D Shading with Box-Shadows</a></li>
+	<li><a href='http://tympanus.net/codrops/2013/08/13/multi-level-push-menu/' title='Multi-Level Push Menu'>Multi-Level Push Menu</a></li>
+	<li><a href='http://tympanus.net/codrops/2013/08/09/building-a-circular-navigation-with-css-transforms/' title='Building a Circular Navigation with CSS Transforms'>Building a Circular Navigation with CSS Transforms</a></li>
+	<li><a href='http://tympanus.net/codrops/2013/08/06/creative-link-effects/' title='Creative Link Effects'>Creative Link Effects</a></li>
+				</ul>
+			</article>
+			<div class="clr spacer"></div>
+		</div>
+		<aside class="ct-sidebar">		
+	<div class="ct-ads">
+		<div class="ct-ads-info">
+			<h4>Advertisement</h4>
+			<a href="/codrops/advertise/" class="ct-advertise-here">
+				<i class="ct-icon-info"></i>
+				<span>Advertise Here</span>
+			</a>
+		</div>
+		<div id="bsap_1275127" class="bsarocks bsap_af25dfd2f1908889af7a1aa5f4dcbd9e"></div>
+		<div id="bsap_1243201" class="bsarocks bsap_af25dfd2f1908889af7a1aa5f4dcbd9e"></div>
+		<div class="clr"></div>
+		<!-- New Relic Campaign -->
+				<div id="nr_campaign">
+			<h5>Codrops recommends:</h5>
+			<SCRIPT language='JavaScript1.1' SRC="http://ad.doubleclick.net/adj/N7384.1045085.TYMPANUS.NETCODROP/B7297634;sz=300x250;ord=1383603295?"></SCRIPT>
+			<NOSCRIPT>
+				<A HREF="http://ad.doubleclick.net/jump/N7384.1045085.TYMPANUS.NETCODROP/B7297634;sz=300x250;ord=1383603295?">
+				<IMG SRC="http://ad.doubleclick.net/ad/N7384.1045085.TYMPANUS.NETCODROP/B7297634;sz=300x250;ord=1383603295?" BORDER=0 WIDTH=300 HEIGHT=250 ALT="Advertisement"></A>
+			</NOSCRIPT>
+		</div>
+		<!-- /New Relic Campaign -->
+	</div>
+		
+	<div class="ct-widget-wrapper">
+									<div class="ct-widget ct-widget-coll">
+					<h5>Latest News:</h5>
+					                <article id="post-16729" class="post-16729 collective type-collective status-publish hentry">
+					<h3><a href="http://tympanus.net/codrops/collective/collective-88/" class="">Collective #88</a></h3>
+                    <div class="ct-latest-thumb">
+                        <a href="http://tympanus.net/codrops/collective/collective-88/" class="ct-trans">
+                            <img width="75" height="105" src="http://codropspz.tympanus.netdna-cdn.com/codrops/wp-content/uploads/2013/11/Collective88.png" class="attachment-thumbnail wp-post-image" alt="Collective88" />                        </a>
+                    </div>
+					
+                    <p class="ct-feat-excerpt">
+                       Snazzy Maps * Musical Keyboard * Ionicons * Boostrap Themes * VSCO Cam effect * Flexbox Video Tutorial * Magic Move...
+						<span> <a href="http://tympanus.net/codrops/collective/collective-88/">read more</a></span>
+                    </p>
+                    <div class="clr"></div>
+                </article>
+                            <article id="post-16676" class="post-16676 collective type-collective status-publish hentry">
+					<h3><a href="http://tympanus.net/codrops/collective/collective-87/" class="">Collective #87</a></h3>
+                    <div class="ct-latest-thumb">
+                        <a href="http://tympanus.net/codrops/collective/collective-87/" class="ct-trans">
+                            <img width="75" height="105" src="http://codropspz.tympanus.netdna-cdn.com/codrops/wp-content/uploads/2013/10/Collective87.png" class="attachment-thumbnail wp-post-image" alt="Collective87" />                        </a>
+                    </div>
+					
+                    <p class="ct-feat-excerpt">
+                       Snap.svg * Cargo Cult CSS * Smart Transitions in UX Design * Zombie Parallax Scrolling * Offline.js * doCapture * Stereo...
+						<span> <a href="http://tympanus.net/codrops/collective/collective-87/">read more</a></span>
+                    </p>
+                    <div class="clr"></div>
+                </article>
+            				</div>
+									<div class="clr"></div>
+			
+		
+		<div class="ct-widget ct-widget-default">
+			<h5>Hot on Codrops:</h5>
+			                <article id="post-16459" class="post-16459 post type-post status-publish format-standard hentry category-playground tag-activity-indicator tag-animation tag-preloader tag-transition">
+					
+                    <div class="ct-latest-thumb">
+                        <a href="http://tympanus.net/codrops/2013/09/18/creative-loading-effects/" class="ct-trans">
+                            <img width="105" height="105" src="http://codropspz.tympanus.netdna-cdn.com/codrops/wp-content/uploads/2013/09/CreativeLoadingEffects-105x105.png" class="attachment-thumbnail wp-post-image" alt="CreativeLoadingEffects" />                        </a>
+                    </div>
+					<h3><a href="http://tympanus.net/codrops/2013/09/18/creative-loading-effects/" class="">Creative Loading Effects</a></h3>
+                   
+                    <div class="clr"></div>
+                </article>
+                            <article id="post-16292" class="post-16292 post type-post status-publish format-standard hentry category-playground tag-3d tag-effect tag-inspiration tag-perspective tag-transition">
+					
+                    <div class="ct-latest-thumb">
+                        <a href="http://tympanus.net/codrops/2013/08/28/transitions-for-off-canvas-navigations/" class="ct-trans">
+                            <img width="105" height="105" src="http://codropspz.tympanus.netdna-cdn.com/codrops/wp-content/uploads/2013/08/sidebartransitions-105x105.png" class="attachment-thumbnail wp-post-image" alt="sidebartransitions" />                        </a>
+                    </div>
+					<h3><a href="http://tympanus.net/codrops/2013/08/28/transitions-for-off-canvas-navigations/" class="">Transitions for Off-Canvas Navigations</a></h3>
+                   
+                    <div class="clr"></div>
+                </article>
+                            <article id="post-16252" class="post-16252 post type-post status-publish format-standard hentry category-playground tag-menu tag-multi-level tag-transform">
+					
+                    <div class="ct-latest-thumb">
+                        <a href="http://tympanus.net/codrops/2013/08/13/multi-level-push-menu/" class="ct-trans">
+                            <img width="105" height="105" src="http://codropspz.tympanus.netdna-cdn.com/codrops/wp-content/uploads/2013/08/MultiLevelPushMenu-105x105.png" class="attachment-thumbnail wp-post-image" alt="MultiLevelPushMenu" />                        </a>
+                    </div>
+					<h3><a href="http://tympanus.net/codrops/2013/08/13/multi-level-push-menu/" class="">Multi-Level Push Menu</a></h3>
+                   
+                    <div class="clr"></div>
+                </article>
+            					</div>
+		
+		<div class="ct-widget" id="md-widget">
+			<h5>From Mightydeals</h5>
+			<div>
+				<script type="text/javascript">
+					(function() {
+					  var url = 'http://www.mightydeals.com/widgets/large?width=280&ref=codropswidget';
+					  document.write('<div id="md-large-widget"></div>');
+					  var s = document.createElement('script'), s1 = document.getElementsByTagName('script')[0];
+					  s.type = 'text/javascript';
+					  s.async = true;
+					  s.src = url;
+					  s1.parentNode.insertBefore(s, s1);
+					})();
+				</script>		
+			</div>
+		</div>
+	</div>
+
+    		
+    	<div class="clr"></div>
+</aside>
+
+		<div class="clr"></div>
+    </div>
+</div>
+		<footer class="ct-footer">
+			
+			<div class="ct-inner ct-cf">
+				<nav class="ct-cf">
+					<ul>
+						<li><a href="/codrops/category/tutorials/">Tutorials</a></li>
+						<li><a href="/codrops/category/articles/">Articles</a></li>
+						<li><a href="/codrops/category/playground/">Playground</a></li>
+						<li><a href="/codrops/category/blueprints/">Blueprints</a></li>
+						<li><a href="/codrops/collective/">Collective</a></li>
+					</ul>
+					<ul>
+						<li><a href="/codrops/category/news/">News</a></li>
+						<li><a href="/codrops/category/giveaways/">Giveaways</a></li>
+						<li><a href="/codrops/category/freebies/">Freebies</a></li>
+					</ul>
+					<ul>
+						<li><a href="/codrops/about/">About</a></li>
+						<li><a href="/codrops/contact/">Contact</a></li>
+						<li><a href="/codrops/archives/">Archives</a></li>
+						<li><a href="/codrops/deals/">Deals</a></li>
+						<li><a href="/codrops/advertise/">Advertise</a></li>
+					</ul>
+					<ul>
+						<li><a href="/codrops/privacy-policy/">Privacy Policy</a></li>
+						<li><a href="/codrops/licensing/">License</a></li>
+						<li><a href="/codrops/credits/">Credits</a></li>
+						<li><a href="/codrops/impressum/">Impressum</a></li>
+					</ul>
+				</nav>
+				<div class="ct-items ct-cf">
+					<div class="ct-item ct-newrelic">
+						<span>Codrops uses</span>
+						<a href="http://goo.gl/78ZCBS" title="New Relic Monitoring" target="blank">Application Performance Monitoring &amp; Management</a>
+					</div>
+					<div class="ct-item ct-cdn">
+						<span>Content delivered by</span>
+						<a href="http://tracking.maxcdn.com/c/24359/3968/378" title="Content Distribution Network" target="blank">Content Distribution Network</a>
+						<img height="1" width="1" src="http://tracking.maxcdn.com/i/24359/3968/378" />
+					</div>
+					<div class="ct-item ct-copyright">
+						<span>&copy; Codrops 2013 by</span> 
+						<a href="http://tympanus.net/" title="Tympanus">Tympanus</a>
+					</div>	
+				</div>
+			</div>
+		</footer>
+				</div><!--ct-container-->
+	<script type="text/javascript">if(!NREUMQ.f){NREUMQ.f=function(){NREUMQ.push(["load",new Date().getTime()]);var e=document.createElement("script");e.type="text/javascript";e.src=(("http:"===document.location.protocol)?"http:":"https:")+"//"+"js-agent.newrelic.com/nr-100.js";document.body.appendChild(e);if(NREUMQ.a)NREUMQ.a();};NREUMQ.a=window.onload;window.onload=NREUMQ.f;};NREUMQ.push(["nrfj","beacon-2.newrelic.com","1caac07779","1957746","M1ADYUVXXENTVEwPVgoaIFZDX11eHQMIUg==",0,817,new Date().getTime(),"","","","",""]);</script></body>
+</html>
+<!-- Performance optimized by W3 Total Cache. Learn more: http://www.w3-edge.com/wordpress-plugins/
+
+Minified using disk
+Page Caching using disk: enhanced (Requested URI contains query)
+Content Delivery Network via codropspz.tympanus.netdna-cdn.com
+
+ Served from: tympanus.net @ 2013-11-04 23:14:55 by W3 Total Cache -->
